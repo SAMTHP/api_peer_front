67f29c4004b5f431d6c41c293d8e29d1a24e0d8d
Samir Founou <samir.founou@avizzeo.com>
2020-10-23 20:32:13 +0200

    MAJ

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php 
-
     header('Access-Control-Allow-Origin: https://api-peer-good.herokuapp.com');
-    
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,18 +11,26 @@
         <title>Document</title>
     </head>
     <body>
-        <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
-            <a href="#" class="navbar-brand">WebRTC</a>
-        </nav>
+        <ul class="nav p-2 shadow mb-5">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Chat</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Documentation</a>
+            </li>
+        </ul>
 
         <div class="container">
+            <h1 class="text-white text-center bg-dark shadow">TP PEER TO PEER</h1>
             <div class="row">
                 <div class="col-sm-6">
-                    <h2>Réception</h2>
+                    <h2 class="text-muted text-center font-weight-bold">Réception</h2>
                     <video controls id="receiver-video" width="100%" height="400px"></video>
                     <p>
-                        <button id="start" class="btn btn-outline-success">Démarrer la conversation</button>
-                        <div class="form-group">
+                        <div class="d-flex justify-content-center">
+                            <button id="start" class="btn btn-outline-success">Démarrer la conversation</button>
+                        </div>
+                        <div class="form-group mt-2">
                             <label for="exampleInputEmail1">Nom de la conversation</label>
                             <input type="text" class="form-control" id="input-offer" placeholder="Ecrivez le nom de la conversation">
                             <small id="emailHelp" class="form-text text-muted">Vous devez spécifier un nom pour la conversation si vous décidez de la créer</small>
@@ -34,16 +39,18 @@
                     <textarea name="" id="offer" class="form-control"></textarea>
                 </div>
                 <div class="col-sm-6">
-                    <h2>Envoi</h2>
+                    <h2 class="text-muted text-center  font-weight-bold">Envoi</h2>
                     <video controls id="emitter-video" width="100%" height="400px"></video>
                     <p>
-                        <button id="receive" class="btn btn-default">Recevoir la conversation</button>
+                        <div class="d-flex justify-content-center">
+                            <button id="receive" class="btn btn-outline-info">Recevoir la conversation</button>
+                        </div>
+                        <div class="form-group mt-2">
+                            <label for="exampleInputEmail1">Nom de la conversation</label>
+                            <input type="text" class="form-control" id="input-receive" placeholder="Ecrivez le nom de la conversation">
+                            <small id="emailHelp" class="form-text text-muted">Indiquez le nom de la conversation</small>
+                        </div>
                     </p>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nom de la conversation</label>
-                        <input type="text" class="form-control" id="input-receive" placeholder="Ecrivez le nom de la conversation">
-                        <small id="emailHelp" class="form-text text-muted">Indiquez le nom de la conversation</small>
-                    </div>
                     <form id="incoming">
                         <textarea id="text-area" class="form-control"></textarea>
                         <!-- <button type="submit" class="btn btn-outline-info">Enregistrer l'offre</button> -->
